1d449a4f1bad0fbd78cf1ca8de38b177f232b062
boshka0 <nastyaboksha@gmail.com>
2018-09-17 18:53:38 +0300

    Implement getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var multiple = 5, lastNum = Math.floor(number/multiple);
+  var zeros = lastNum;
+  while(lastNum){
+    multiple *= 5;
+    lastNum = Math.floor(number/multiple);
+    zeros += lastNum;
+  }
+  return zeros;
 }
